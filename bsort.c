c8c931b176e6dfe7ac1f4bf8bf825ed5e4094d6e
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2022-08-09 22:07:40 -0300

    Update bsort.c

--- bsort.c
+++ bsort.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "biblioteca.h"
+long long cpomp=0; 
 int comp=0;
 void bsort(int v[], int n)
 {
@@ -9,50 +10,28 @@ void bsort(int v[], int n)
 	{
 		for(j=0;j<n-i; j++)
 		{
-			if(v[j]>v[j+1])
-			{
-				troca(&v[j],&v[j+1]);
-			}//fim for if
-		}// fim for j
-	}// fim for i
-}
-//----------------------------
-void bsortComentado(int v[], int n)
-{
-	int i, j;
-	for(i=1;i<n;i++)//fases
-	{
-		printf("Fase %d\n",i);getch();//
-		printVetor(v,n);//
-		for(j=0;j<n-i; j++)//comparações
-		{
-			printf("%d>%d",v[j],v[j+1]);
 			comp++;
 			if(v[j]>v[j+1])
 			{
-				printf("Trocando %d com %d\n",v[j],v[j+1]);
 				troca(&v[j],&v[j+1]);
-				printVetor(v,n);
 			}//fim for if
 		}// fim for j
 	}// fim for i
 }
-//----------------------------
+
 int main()
 {
 	int *vet, i, n;
+	float ini, fim, tempo; 
 	printf("Tamanho do vetor: ");
 	scanf("%d", &n);
 	vet = malloc(sizeof(int)*n);//alocando vetor dinamicamente
-	//geraVet(vet,n);
-	printf("Dados para o vetor: ");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d", &vet[i]);
-    }
-	printVetor(vet,n);
-	bsortComentado(vet,n);
-	printf("Vetor ordenado:\n");
-	printVetor(vet,n);
+	
+	ini = clock();
+	bsort(vet,n);
+	fim = clock();
+	tempo = (fim - ini)/1000;
+
+	printf("Tempo(s):%.4f\n", tempo);
 	printf("Qtde de comparação:%d\n",comp);
 }
